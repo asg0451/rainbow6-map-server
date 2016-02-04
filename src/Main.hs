058c1c84f36c7aaedff45b8bb38d8b0d1f1c9690
Miles Frankel <miles.frankel@gmail.com>
2016-02-04 14:00:32 -0500

    using statet to hold list of all maps

--- src/Main.hs
+++ src/Main.hs
@@ -8,6 +8,7 @@ import           Web.Scotty                           as S
 
 import           Control.Monad
 import           Control.Monad.IO.Class
+import           Control.Monad.State.Lazy             as ST
 import           Data.List                            (sortBy)
 import           Data.List.Split                      (chunksOf)
 import           Safe                                 (readDef)
@@ -16,65 +17,61 @@ import           System.FilePath
 
 import qualified Data.Text                            as T
 import           Lucid
-import           Lucid.Base
 
-lucid = S.raw . renderBS
+
+type MapHtml = HtmlT (StateT MapState IO)
+
+type MapState = [String] -- list of map names
+
+lucid :: MapHtml () -> ActionM ()
+lucid h = do
+  maps <- liftIO $ getDirectoryContents "maps/"
+  let maps' = filter (not . (`elem` [".", ".."])) maps
+      bst = renderBST h
+  bst' <- liftIO $ evalStateT bst maps'
+  S.raw bst'
 
 main :: IO ()
 main =
     scotty 4646 $
     do middleware logStdoutDev
        middleware $ staticPolicy $ hasPrefix "maps/" <|> hasPrefix "static/"
-       get "/map/:map" $
+       S.get "/" $ redirect "/map/yacht"
+       S.get "/map/:map" $
            do setHeader "Content-Type" "text/html"
               mapSelection <- S.param "map"
-              when ('/' `elem` mapSelection) next
-              files <- liftIO $ getDirectoryContents $ "maps/" </> mapSelection
-              let files' =
-                      filter
-                          (\f ->
-                                f /= "." && f /= "..")
-                          files
-                  files'' =
-                      sortBy
-                          (\a b ->
-                                let aName = takeBaseName a
-                                    bName = takeBaseName b
-                                    na = readDef 0 $ init aName :: Int
-                                    nb = readDef 0 $ init bName :: Int
-                                in compare na nb)
-                          files'
-                  filesWithFloor =
+              when ('/' `elem` mapSelection) $ redirect "/"
+              fs <- liftIO $ getDirectoryContents $ "maps/" </> mapSelection
+              let filesWithFloor =
                       map
                           (\f ->
-                                (takeBaseName f, f))
-                          files''
-              maps <- liftIO $ getDirectoryContents "maps/"
-              let maps' =
-                      filter
-                          (\m ->
-                                m /= "." && m /= "..")
-                          maps
+                                (takeBaseName f, f)) $
+                      sortBy floorCmp $ filter (not . (`elem` [".", ".."])) fs
+                  chunkedFiles = chunksOf 2 filesWithFloor
               lucid $
-                  template maps' ("map viewer: " ++ mapSelection) $
-                  do div_ [class_ "container-fluid"] $
-                         do let chunkedFiles = chunksOf 2 filesWithFloor
-                                toCol :: (String, FilePath) -> Html ()
-                                toCol (name,f) =
-                                    div_ [class_ "col-md-6"] $
-                                    do div_ [class_ "col-md-12"] $
-                                           h3_ $ toHtml name
-                                       img_
-                                           [ src_ $
-                                             T.pack $
-                                             "/maps" </> mapSelection </> f
-                                           , class_ "img-responsive"]
-                            forM_ chunkedFiles $
-                                (div_ [class_ "row"]) . mapM toCol
+                  do template ("map viewer: " ++ mapSelection) $
+                         do div_ [class_ "container-fluid"] $
+                                forM_ chunkedFiles $
+                                (div_ [class_ "row"]) .
+                                mapM (toCol mapSelection)
+  where
+    floorCmp a b =
+        let aName = takeBaseName a
+            bName = takeBaseName b
+            na = readDef 0 $ init aName :: Int
+            nb = readDef 0 $ init bName :: Int
+        in compare na nb
+    toCol :: String -> (String, FilePath) -> MapHtml ()
+    toCol mapSel (name,f) =
+        div_ [class_ "col-md-6"] $
+        do div_ [class_ "col-md-12"] $ h3_ $ toHtml name
+           img_
+               [ src_ $ T.pack $ "/maps" </> mapSel </> f
+               , class_ "img-responsive"]
 
 
-template :: [String] -> String -> Html () -> Html ()
-template ms title body = do
+template :: String -> MapHtml () -> MapHtml ()
+template title bod = do
     doctype_
     html_ [lang_ "en"] $
         do head_ $
@@ -85,22 +82,23 @@ template ms title body = do
                   title_ $ toHtml title
                   includes
            body_ $
-               do body
-                  theFooter ms
+               do bod
+                  theFooter
 
-theFooter :: [String] -> Html ()
-theFooter ms =
+theFooter :: MapHtml ()
+theFooter =
     nav_ [class_ "navbar navbar-default navbar-fixed-bottom"] $
     do div_ [class_ "container"] $
            do div_ [class_ "footer"] $
-                  do forM_ ms $
+                  do ms <- lift ST.get
+                     forM_ ms $
                          \m ->
                               a_
                                   [ class_ "navbar-link"
                                   , href_ (T.pack $ "/map/" ++ m)] $
                               toHtml m
 
-includes :: Html ()
+includes :: MapHtml ()
 includes = do
     link_
         [ rel_ "stylesheet"
@@ -119,5 +117,5 @@ includes = do
         , rel_ "stylesheet"
         , type_ "text/css"]
 
-scriptIncl_ :: [Attribute] -> Html ()
+scriptIncl_ :: [Attribute] -> MapHtml ()
 scriptIncl_ as = script_ as $ T.pack ""
